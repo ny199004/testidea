4cd556dfe51cb426cc241d9c97d16f0c1679c304
聂野 <2691439293@qq.com>
2019-06-09 16:30:12 +0800

    add Demo1.java

---
+++ testhttpUrlConnection/src/main/java/cn/ny/Demo1.java
@@ -0,0 +1,13 @@
+package cn.ny;
+
+/**
+ * @author nieye
+ * @create 2019-06-09 16:28
+ */
+public class Demo1 {
+
+    public static void main(String[] args) {
+        System.out.println("nihao  git");
+    }
+
+}
